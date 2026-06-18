1f69cb2e807353b6c331d205a4b1d24c51dbb4b6
Jared-Six <jassomjared@gmail.com>
2026-06-18 13:45:04 -0700

    completed scrum space 26 and 27

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpattherns = [
+    path('', views.home, name='home'),
+
+]
--- calorieCounter/settings.py
+++ calorieCounter/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # name for app may change later
+
+    'CalorieCounterApp', 
 ]
 
 MIDDLEWARE = [
