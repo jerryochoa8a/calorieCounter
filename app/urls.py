e1bad19f17122b61d6fc99666ce3a7dd8ab6d56d
Jerry <jerryochoa8a@aol.com>
2026-07-20 13:51:35 -0700

    reg and login error message test.

--- app/urls.py
+++ app/urls.py
@@ -3,15 +3,16 @@ from . import views
 
 urlpatterns = [
     # Pages
-    path('', views.loginPage, name='loginPage'),
+    path('', views.loginPage, name='loginPage'), ##website opens to the login page
     path('regPage', views.regPage, name='regPage'),
-    path('home', views.home),
+    path('base', views.base, name="base"), 
+    path('home', views.home, name='home'), 
+    path('about', views.about, name='about'), 
 
     # Functions
-    path('new_user', views.new_user), 
-    path('user_login', views.user_login),
-    path('home/logout', views.logout), 
-    
+    path('new_user', views.new_user), ## registers User
+    path('user_login', views.user_login), ## Login User
+    path('home/logout', views.logout), ##logout User    
 
 ]
 
