b30b34589b9884ee022d37bed4a8000df5b7d9ae
Jared-Six <jassomjared@gmail.com>
2026-07-30 18:12:22 -0700

    added class models, target calories function, and pydoc for models.py and views.py

--- app/models.py
+++ app/models.py
@@ -1,20 +1,207 @@
+"""
+Name: models.py
+Date: 7/30/2026
+Programmers: Jared Jasso and Jerry Ochoa
+
+This module contains the models for our calorie counter app. The models represent the apps data which include 
+users, profiles, target calories, food logs, food, and food log entries. We use these models to store, retrieve,
+and manage the data for this app. 
+
+"""
+
 from django.db import models
 
 # Create your models here.
 
 class User(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    email = models.CharField(max_length=40)
-    # weight = models.IntegerField()
-    # height = models.IntegerField()
-    # age = models.IntegerField()
-    # gender = models.CharField()
-    password = models.CharField(max_length=50)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+     """
+     Class Name: User
+     Programmer: Jerry Ochoa
 
-#food
+     Stores the users information such as first name, last name, email, and password. It also records the date and 
+     time when the user was created and last updated.
 
-#foodlog
+     """
+
+     first_name = models.CharField(max_length=20)
+     last_name = models.CharField(max_length=20)
+     email = models.CharField(max_length=40)
+     # weight = models.IntegerField()
+     # height = models.IntegerField()
+     # age = models.IntegerField()
+     # gender = models.CharField()
+     password = models.CharField(max_length=50)
+
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+class Profile(models.Model):
+
+     """
+     Class Name: Profile
+     Programmer: Jared Jasso
+
+     It has a one-to-one relationship with the User model, meaning each user can have only one profile
+     and if the user is deleted, the profile will also be deleted.
+
+     Stores more detailed information about the user such as weight, height, age, sex, activity levels, and 
+     fitness goals.
+
+     """
+
+     user = models.OneToOneField(User, on_delete=models.CASCADE)
+     weight = models.IntegerField()
+     height = models.IntegerField()
+     age = models.IntegerField()
+
+     sexChoices = [
+        ("Male", "Male"),
+        ("Female", "Female"),
+     ]
+
+     sex = models.CharField(
+        max_length = 6,
+        choices = sexChoices
+     )
+
+
+     activityLevelChoices = [
+        ("Sedentary", "Sedentary"),
+        ("Lightly Active", "Lightly Active"),
+        ("Moderately Active", "Moderately Active"),
+        ("Very Active", "Very Active"),
+     ]
+
+     activityLevels = models.CharField(
+        max_length = 20,
+        choices = activityLevelChoices
+     )
+
+
+     fitnessGoalChoices = [
+        ("Lose Weight", "Lose Weight"),
+        ("Maintain Weight", "Maintain Weight"),
+        ("Gain Weight", "Gain Weight"),
+     ]
+
+     fitnessGoals = models.CharField(
+        max_length = 20,
+        choices = fitnessGoalChoices
+     )
+
+class TargetCalories(models.Model):
+
+     """
+     Class Name: TargetCalories
+     Programmer: Jared Jasso
+
+     It has a one-to-one relationship with the Profile model, meaning each profile can 
+     have only have one target calories at a time. 
+     
+     Stores the target calories for a user based on their profile information. It also 
+     records the date and time when the target calories were created and last updated.
+
+     """
+
+     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+     target_calories = models.IntegerField()
+
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+class Food(models.Model):
+
+     """
+     Class Name: Food
+     Programmer: Jared Jasso
+
+     Stores information about food items such as name, calories, protein, carbs, and fat. 
+     This will get information from api "calorieninjas" 
+
+     The "__str__" method returns a readable string.
+
+     """
+
+     name = models.CharField(max_length=100)
+
+     calories = models.IntegerField()
+     protein = models.FloatField()
+     carbs = models.FloatField()
+     fat = models.FloatField()
+
+     def __str__(self):
+        return self.name
+
+class FoodLog(models.Model):
+
+     """
+     Class Name: FoodLog
+     Programmer: Jared Jasso
+
+     Represents a food log for a user of a specific date. It uses a one to many relationship with profile. 
+     It also stores the date of the log, and maintains a many-to-many relationship with the Food model 
+     through the FoodLogEntry model. 
+
+     The "calculateTotalCalories" method calculates the total calories consumed in the food log using 
+     the FoodLogEntry model.
+
+     The "__str__" method returns a readable string of user name and date of foodlog.
+
+     """
+
+     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+     date = models.DateField()
+
+     foods = models.ManyToManyField(Food, through="FoodLogEntry")
+
+     def calculateTotalCalories(self):
+        totalCalories = 0
+
+        for entry in self.foodlogentry_set.all():
+            totalCalories += entry.calculateCalories()
+
+            return totalCalories
+
+     def __str__(self):
+         return (
+            f"{self.profile.user.first_name}'s "
+            f"Food log - {self.date}"
+        )
+
+class FoodLogEntry(models.Model):
+
+     """
+     Class Name: FoodLogEntry
+     Programmer: Jared Jasso
+
+     Represents a single food item that has been entered into a food log. 
+
+     It has a many to one relationship with the FoodLog model and the Food model.
+     If the food log or food item is deleted, the corresponding food log entry will also be deleted.
+
+     The float field represents the quantity of the food item consumed.
+
+     The "calculateCalories" method calculates the total calories for the food item 
+     based on its quantity.
+
+     The "__str__" method returns a readable string of the food name and quantity.
+
+     """
+
+     foodLog = models.ForeignKey(FoodLog, on_delete=models.CASCADE)
+
+     food = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+     quantity = models.FloatField(default=1)   
+
+     def calculateCalories(self):
+        return self.food.calories * self.quantity
+
+     def __str__(self):
+        return (
+            f"{self.food.name} - "
+            f"{self.quantity} serving(s)"
+            )
--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
+"""
+Module Name: views.py
+Date: 7/30/2026
+Programmers: Jerry Ochoa and Jared Jasso
+
+This module contains the functions for the calorie counter app. It handles page navigation, 
+user registration, login/logout, food searches using an external API, food log managenment
+and target calorie calculations.   
+
+"""
 from django.shortcuts import render, redirect
-from app.models import User
+from app.models import User, Profile, TargetCalories, FoodLog, Food
 from django.contrib import messages
 from django.http import HttpResponse
 import bcrypt
@@ -9,8 +19,15 @@ from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 
 
-### PAGES ##########
 
+### PAGES ##########
+"""
+Function Name: loginPage, regPage, base, home, about
+Programmer: Jerry Ochoa
+
+Displays the corresponding page when the user navigates to the URL. It renders the HTML 
+template for each page and passes any necessary context data to the template.
+"""
 def loginPage(request):
     return render(request, "loginPage.html")
 
@@ -41,127 +58,181 @@ def about(request):
 # Creating a new user
 def new_user(request):
 
-        # email errors:
-        email = request.POST['email']
+     """
+     Function Name: new_user
+     Programmer: Jared Jasso
 
-        # validates correct email format
-        try:
-            validate_email(email)
-        except ValidationError:
-            return render(request, "regPage.html", {"error": "Please enter a valid email address."})
+     Handles the registration of a new user. It stores different error messages in case new user input is invalid.
+     If new user input is valid, it creates a new user in the database with the first name, last name, email, and 
+     hashed password. It then redirects the user to the base page after successful registration.
+     """
 
-        # validates if email is already in use
-        if User.objects.filter(email=email).exists():
-            return render(request, "regPage.html", {"error": "Email is already in use."})
+     # email errors:
+     email = request.POST['email']
 
-        # first name errors:
-        first_name = request.POST['fname']
+     # validates correct email format
+     try:
+         validate_email(email)
+     except ValidationError:
+         return render(request, "regPage.html", {"error": "Please enter a valid email address."})
 
-        # first name is not empty
-        if not first_name:
-            return render(request, "regPage.html", {"error": "First name cannot be empty."})
+     # validates if email is already in use
+     if User.objects.filter(email=email).exists():
+         return render(request, "regPage.html", {"error": "Email is already in use."})
 
-        # last name errors:
-        last_name = request.POST['lname']
+     # first name errors:
+     first_name = request.POST['fname']
 
-        # last name is not empty
-        if not last_name:
-            return render(request, "regPage.html", {"error": "Last name cannot be empty."})
+     # first name is not empty
+     if not first_name:
+         return render(request, "regPage.html", {"error": "First name cannot be empty."})
 
-        # password errors:
-        password = request.POST['password']
+     # last name errors:
+     last_name = request.POST['lname']
 
-        # password is not empty
-        if not password:
-            return render(request, "regPage.html", {"error": "Password cannot be empty."})
+     # last name is not empty
+     if not last_name:
+         return render(request, "regPage.html", {"error": "Last name cannot be empty."})
 
-        # password must be at least 8 characters long
-        if len(password) < 8:
-            return render(request, "regPage.html", {"error": "Password must be at least 8 characters long."})
+     # password errors:
+     password = request.POST['password']
 
-        # hash the password
-        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+     # password is not empty
+     if not password:
+         return render(request, "regPage.html", {"error": "Password cannot be empty."})
 
-        # creates the user in the database
-        User.objects.create(
-            first_name = request.POST['fname'],
-            last_name = request.POST['lname'],
-            email = request.POST['email'],
-            password = pw_hash
+     # password must be at least 8 characters long
+     if len(password) < 8:
+         return render(request, "regPage.html", {"error": "Password must be at least 8 characters long."})
+
+     # hash the password
+     pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+     # creates the user in the database
+     user = User.objects.create(
+         first_name = request.POST['fname'],
+         last_name = request.POST['lname'],
+         email = request.POST['email'],
+         password = pw_hash
         )
-        print(User.objects.all())
+     print(User.objects.all())
 
-        return redirect('/base')
+     return redirect('/base')
 
 
 # Logging in an existing user
 def user_login(request):
-    user = User.objects.filter(email=request.POST['login_email'])
-    if user:
-        logged_user = user[0]
-        if bcrypt.checkpw(request.POST['login_pw'].encode(), logged_user.password.encode()):
-            request.session['userid'] = logged_user.id
-            return redirect('/base')
-    else:
-        errorMessage = {"error": "Wrong email or password"}
-        return render(request,'loginPage.html', errorMessage) # This will redirect to home page if the user login failed 
+
+     """
+     Function Name: user_login
+     Programmer: Jared Jasso
+
+     Function handles the login of an existing user. It checks if
+     the submitted email matches an existing user, if it does 
+     it checks if the submitted password matches the hashed 
+     password stored in the database. If it does not, it shows an 
+     error message, if it does it redirects the user to the base page
+     and stores the user's ID in the session.
+
+     """
+     user = User.objects.filter(email=request.POST['login_email'])
+     if user:
+         logged_user = user[0]
+         if bcrypt.checkpw(request.POST['login_pw'].encode(), logged_user.password.encode()):
+             request.session['userid'] = logged_user.id
+             return redirect('/base')
+         else:
+             errorMessage = {"error": "Wrong email or password"}
+             return render(request,'loginPage.html', errorMessage) # This will redirect to home page if the user login failed 
 
 
 # Logging out a user
 def logout(request):
-    request.session.pop('userid', None) # Takes the user out of the session
-    messages.error(request, "You have successfully logged out!")
-    return redirect('/')
+
+     """
+     Function Name: logout
+     Programmer: Jared Jasso
+    
+     Function handles the logout of a user. It removes the user's ID from the session and displays a message
+     indicating that the user has successfully logged out. It then redirects the user to the login page.
+    
+     """
+
+     request.session.pop('userid', None) # Takes the user out of the session
+     messages.error(request, "You have successfully logged out!")
+     return redirect('/')
 
 
 #food API search
 def GetfoodInfo(request):
-    # Search = Amount + Food
-    amount = request.POST['amount']
-    food = request.POST['food']
-    search = amount + food
 
-    api_key = settings.calorieninjas_APIKey
+     """
+     Function Name: GetfoodInfo
+     Programmer: Jerry Ochoa
 
-    url = "https://api.calorieninjas.com/v1/nutrition"
-    headers = {
+     Function searches for food information using the CalorieNinjas API.
+     It converts the JSON response into a Python dictionary, and passes
+     the nutritional information to a template. 
+
+     """
+
+     # Search = Amount + Food
+     amount = request.POST['amount']
+     food = request.POST['food']
+     search = amount + food
+
+     api_key = settings.calorieninjas_APIKey
+
+     url = "https://api.calorieninjas.com/v1/nutrition"
+     headers = {
         "X-Api-Key": f"{api_key}"
-    }
-    params = {
+     }
+     params = {
         "query": f"{search}",
         #Ex) "query": "1 eggs",
-    }
-    response = requests.get(url,headers=headers, params=params)
-    data = response.json()
+     }
+     response = requests.get(url,headers=headers, params=params)
+     data = response.json()
 
-    #Just display to the page to alow the user to confirm info -> addTo_foodlog()    
-    foodInfo = {
+     #Just display to the page to alow the user to confirm info -> addTo_foodlog()    
+     foodInfo = {
         "name": data["items"][0]["name"], #str, 
         "calories": data["items"][0]["calories"], #int
         "protein":data["items"][0]["protein_g"], #int,
         "carbs":data["items"][0]["carbohydrates_total_g"], #int
         "fiber":data["items"][0]["fiber_g"], #int
         "fat":data["items"][0]["fat_total_g"], #int
-    }
+     }
 
-    return render(request,'html', foodInfo) # ! once page is avalable update
+     return render(request,'html', foodInfo) # ! once page is avalable update
     
     
 def addTo_Foodlog(foodInfo):
-    #making the food object
-    foodItem = Food.objects.create(
+
+     """
+     Function Name: addTo_Foodlog
+     Programmer: Jerry Ochoa
+
+     Function creates food object and adds it to the food log for the user.
+     It checks if a food log exists for the user on the current date, and if 
+     not, it creates a new food log. It then adds the food item to the food log.
+
+     """
+
+     #making the food object
+     foodItem = Food.objects.create(
         name =foodInfo.name, #str
         calories = foodInfo.calories, #int
         protein = foodInfo.protein, #int
         carbs = foodInfo.carbs, #int
         fiber = foodInfo.fiber, #int
         fat = foodInfo.fat, #int
-    )
-    # get the food log with the the users ID and todays date
-    foodLog = FoodLog.objects.filter(userID=request.POST[''], date="todays date" )
-    # [if] there is no foodLog found then make one(userID, Date) -> createFoodlog()
+     )
+     # get the food log with the the users ID and todays date
+     foodLog = FoodLog.objects.filter(userID=request.POST[''], date="todays date" )
+     # [if] there is no foodLog found then make one(userID, Date) -> createFoodlog()
 
-    foodLog.log = foodItem_id
+     foodLog.log = foodItem_id
 
 
 # def createFoodlog(userID, Date):
@@ -171,3 +242,64 @@ def addTo_Foodlog(foodInfo):
 #         date = todays date,
 #     )
 
+# function that calculates target calories 
+
+def calculateTargetCalories(request, profile_id):
+    
+    """
+    Function Name: calculateTargetCalories
+    Programmer: Jared Jasso
+
+    Function calculates the target calories for a user based on their profile information.
+    It calculates the Basal Metabolic Rate (BMR) based on the user's sex, weight, height,
+    and age. It then applies an activity level multiplier based on the user's activity level.
+    Finally, it adjusts the target calories based on the user's fitness goal (lose weight, 
+    gain weight, or maintain weight). 
+
+    The function uses the Mifflin-St Jeor equation to calculate BMR and returns the target calories as an integer.
+
+    Female BMR = (10 * weight) + (6.25 * height) - (5 * age) - 161
+    Male BMR = (10 * weight) + (6.25 * height) - (5 * age) + 5
+
+    """
+
+    profile = Profile.objects.get(id=profile_id)
+
+    # calculates body mass rate (BMR) based on sex, weight, height, and age
+
+    if profile.sex == "Female":
+        BMR = (10 * profile.weight) + (6.25 * profile.height) - (5 * profile.age) - 161
+    else:
+        BMR = (10 * profile.weight) + (6.25 * profile.height) - (5 * profile.age) + 5
+
+    # activity level multipliers
+
+    if profile.activityLevels == "Sedentary":
+        activityMultiplier = 1.2
+
+    elif profile.activityLevels == "Lightly Active":
+        activityMultiplier = 1.375
+
+    elif profile.activityLevels == "Moderately Active":
+        activityMultiplier = 1.55
+
+    elif profile.activityLevels == "Very Active":
+        activityMultiplier = 1.725
+
+    # calculates target calories based on BMR, activity level, and fitness goal
+
+    targetCalories = BMR * activityMultiplier
+
+    # adjusts target calories based on fitness goal
+
+    if profile.fitnessGoals == "Lose Weight":
+        targetCalories -= 500
+
+    elif profile.fitnessGoals == "Gain Weight":
+        targetCalories += 500
+
+    elif profile.fitnessGoals == "Maintain Weight":
+        targetCalories = targetCalories
+
+    return targetCalories
+    
